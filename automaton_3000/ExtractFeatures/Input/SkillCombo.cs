7c9c58f0a96a9c560f4f442a28c373bbcddd6b0c
cBashTn <sebastian.mojado@gmail.com>
2021-04-26 17:16:33 +0200

    Cleaned the code a little

---
+++ automaton_3000/ExtractFeatures/Input/SkillCombo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtractFeatures.Input
+{
+    /// <summary>
+    /// Bit 0 : CMD
+    /// Bit 1 : DIP
+    /// Bit 2 : ENG
+    /// Bit 3 : MED
+    /// Bit 4 : SCI
+    /// Bit 5 : SEC
+    /// </summary>
+    [Flags]
+    public enum SkillCombo
+    {
+        Command     = 1,
+        Diplomacy   = 2,
+        Engineering = 4,
+        Medicine    = 8,
+        Science     = 16,
+        Security    = 32
+    }
+}
---
+++ automaton_3000/ExtractFeatures/Input/SkillComboRarityPerCrewRarity.cs
@@ -0,0 +1,18 @@
+﻿namespace ExtractFeatures.Input
+{
+    public class SkillComboRarityPerCrewRarity
+    {
+        public SkillCombo SkillCombo { get; set; }
+
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Count of max stars
+        /// </summary>
+        public int CrewRarity { get; set; }
+
+        public float Rarity { get; set; }
+
+        public float RarityPerCrewRarity { get; set; }
+    }
+}
